63762357bca2789af365faac8315e28099141730
Jakob Meier <jakobmeier@bluewin.ch>
2016-10-25 11:15:56 +0200

    Added Unit test to message parsing

--- app/src/test/java/ch/ethz/inf/vs/a3/pascalo/vs_pascalo_chat/ExampleUnitTest.java
+++ app/src/test/java/ch/ethz/inf/vs/a3/pascalo/vs_pascalo_chat/ExampleUnitTest.java
@@ -2,6 +2,11 @@ package ch.ethz.inf.vs.a3.pascalo.vs_pascalo_chat;
 
 import org.junit.Test;
 
+import java.util.UUID;
+
+import ch.ethz.inf.vs.a3.message.MessageTypes;
+import ch.ethz.inf.vs.a3.solution.message.Message;
+
 import static org.junit.Assert.*;
 
 /**
@@ -13,5 +18,14 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void MessageParsing() throws Exception {
+        Message msg = new Message();
+        msg.set_header("Tester name", UUID.randomUUID().toString(), "1-1-1", MessageTypes.CHAT_MESSAGE);
+
+        Message parsed_msg = new Message(msg.get_json());
+        assert(msg.toString().equals(parsed_msg.toString()));
     }
 }
