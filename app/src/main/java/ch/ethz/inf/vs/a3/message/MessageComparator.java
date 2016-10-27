19e342a96c317af6a67f9a53b29c95a8d762b689
Jakob Meier <jakobmeier@bluewin.ch>
2016-10-27 08:58:11 +0200

    Implemented comparator for messages

--- app/src/main/java/ch/ethz/inf/vs/a3/message/MessageComparator.java
+++ app/src/main/java/ch/ethz/inf/vs/a3/message/MessageComparator.java
@@ -1,6 +1,8 @@
 package ch.ethz.inf.vs.a3.message;
 
 import java.util.Comparator;
+
+import ch.ethz.inf.vs.a3.solution.clock.VectorClock;
 import ch.ethz.inf.vs.a3.solution.message.Message;
 
 /**
@@ -11,8 +13,23 @@ public class MessageComparator implements Comparator<Message> {
     @Override
     public int compare(Message lhs, Message rhs) {
         // Write your code here
-        //TODO Do actual vector clock comparison instead of lexicographic comparison
-        return lhs.header.timestamp.compareTo(rhs.header.timestamp);
+
+        // Converting timestamp to VectorClock
+        VectorClock left_time = new VectorClock();
+        left_time.setClockFromString(lhs.header.timestamp);
+
+        VectorClock right_time = new VectorClock();
+        right_time.setClockFromString(rhs.header.timestamp);
+
+        // set flags left-before-right (lbr) and right-before-left (rbl)
+        boolean lbr = left_time.happenedBefore(right_time);
+        boolean rbl = right_time.happenedBefore(left_time);
+
+        // -1 <=> left less than right
+        // 0  <=> equal
+        // 1 <=> left greater than right
+        return lbr ? -1 : rbl ? 1 : 0;
+        // Yes, cryptic looking code is strictly cooler and faster than easy-to-understand-code :)
     }
 
 }
